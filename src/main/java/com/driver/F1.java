136f23d60a07e6b8dcc6507c267010b2f0d7913f
unknown <kalpeshpatil19999@gmail.com>
2023-08-28 16:11:11 +0530

    vehicle

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -25,15 +25,15 @@ public class F1 extends Car {
         if(newSpeed == 0) {
             stop();
             changeGear(1);
-        }else if(newSpeed > 0 && newSpeed<51) {
+        }else if(newSpeed >=1 && newSpeed<=50) {
             changeGear(1);
-        }else if(newSpeed>50 && newSpeed < 101) {
+        }else if(newSpeed>=51 && newSpeed <= 100) {
             changeGear(2);
-        }else if(newSpeed>100 && newSpeed < 151) {
+        }else if(newSpeed>=101 && newSpeed <= 150) {
             changeGear(3);
-        }else if(newSpeed>150 && newSpeed < 201) {
+        }else if(newSpeed>=151 && newSpeed <= 200) {
             changeGear(4);
-        }else if(newSpeed>200 && newSpeed < 251) {
+        }else if(newSpeed>=201 && newSpeed <= 250) {
             changeGear(5);
         }else if (newSpeed >250){
             changeGear(6);
